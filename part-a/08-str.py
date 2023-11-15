e571f972026689e26c24fda9c115d61abbecb39b
saivishnu725 <saivishnu725@gmail.com>
2023-11-15 12:35:12 +0530

    String functions

---
+++ part-a/08-str.py
@@ -0,0 +1,13 @@
+s1 = "hello world!"
+
+print("upper: " + s1.upper())
+print("lower: " + s1.lower())
+print("capitalize: " + s1.capitalize())
+print("count: " + str(s1.count('e')))
+print("isdigit: " + str(s1.isdigit()))
+print("isupper: " + str(s1.isupper()))
+print("islower: " + str(s1.islower()))
+print("replace o with i: " + s1.replace('o', 'i'))
+print("title: " + s1.title())
+print("split:", s1.split())
+
